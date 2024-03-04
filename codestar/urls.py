a77d4a9e31c27fab331d00e67f6e8ac9dae7d05e
mdurmus <mdurmus@outlook.com>
2024-03-04 17:09:55 +0000

    works

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from blog import views as blog_views
+from blog.views import my_blog
+
 urlpatterns = [
-    path('', blog_views.index, name="index"),
+    path('blog/', my_blog, name="blog"),
     path('admin/', admin.site.urls),
 ]
