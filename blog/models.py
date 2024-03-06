9dfa1715c87bcf82459d22670b03b3b5567a3186
mdurmus <mdurmus@outlook.com>
2024-03-06 10:18:57 +0000

    added line break

--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
