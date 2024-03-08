1242694e4ea12fec528f1742e0474c0adb4283a1
mehmet durmus <mdurmus@outlook.com>
2024-03-08 10:44:18 +0000

    safe test

--- codestar/settings.py
+++ codestar/settings.py
@@ -18,7 +18,7 @@ if os.path.isfile('env.py'):
     import env
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
