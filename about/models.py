4d950105d90d1584399cf8e3e9ed29026d778ef0
mehmet durmus <mdurmus@outlook.com>
2024-03-13 08:53:13 +0000

    final step

--- about/models.py
+++ about/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class About(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=False)
     updated_on = models.DateTimeField(auto_now_add=True)
+    profile_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.title
