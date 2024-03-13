4046be89eab9540043fd7876de69b897af438a08
mehmet durmus <mdurmus@outlook.com>
2024-03-13 06:41:19 +0000

    before cloudinary

--- about/admin.py
+++ about/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,4 +8,7 @@ class AboutAdmin(SummernoteModelAdmin):
     summernote_fields = ('content',)
 
 
-# Register your models here.
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
